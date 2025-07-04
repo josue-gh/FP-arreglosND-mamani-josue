6e4e26a001d32f7282e831298fa81fb53e536912
josue <josue.mlpro.2014@gmail.com>
2025-07-04 09:20:54 -0500

    avance 2- ejercicio 5- pidiendo datos de la matriz

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 main()
 {
+ int matriz[4][4];
  
+ cout<<"ingrese los valores de la matriz 4x4 "<<endl;
+ for(int i=0; i<4; i++){
+ 	for (int j=0; j<4; j++){
+ 		cout<<"fila "<<i<<" columna "<<j<<" : ";
+ 		cin>>matriz[i][j];
+	 }
+	 cout<<endl;
+ }
+ 
+ return 0;
 } 

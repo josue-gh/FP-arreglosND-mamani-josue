ec058d6cd4ffbd79c4b34f3cc0a64360a3d2a445
josue <josue.mlpro.2014@gmail.com>
2025-07-04 23:18:15 -0500

    avance 2- ejercicio 7- pidiendo datos y mostrando matriz dada

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 main()
 {
+ int matriz[4][4];
+ cout<<"ingrese los datos de la matriz 4x4 "<<endl;
+ for(int i=0; i<4; i++){
+ 	for(int j=0; j<4; j++){
+ 		cout<<"ingrese el valor de la fila "<<i<<" columna "<<j<<" ";
+ 		cin>>matriz[i][j];
+	 }
+	 cout<<endl;
+ }
  
+ cout<<"la matriz dada es: "<<endl;
+ for(int i=0; i<4; i++){
+ 	for(int j=0; j<4; j++){
+ 		cout<<matriz[i][j]<<" ";
+	 }
+	 cout<<endl;
+ }
+ 
+ cout<<"la matriz rotada 90 grados es: "<<endl;
+   for(int i=3; i>=0; i--){
+ 	for(int j=0; j<4; j++){
+ 		cout<<matriz[i][j]<<endl;
+	 }
+	 cout<<" ";
+ }
 } 

213731f7e2be4814f1131b583d1736358f21805b
josue <josue.mlpro.2014@gmail.com>
2025-07-04 22:55:07 -0500

    version final- ejercicio 6- reflejo de una matriz

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -3,4 +3,31 @@ using namespace std;
 main()
 {
  
+ int matriz[6][6] = {
+ {12 , 54 , 6  , 9  , 7  , 6},
+ {0 ,  13 , 87 , 99 , 19 , 3},
+ {43 , 3  , 65 , 77 , 81 , 22},
+ {9  , 78 , 66 , 45 , 65 , 1},
+ {2  , 7  , 34 , 76 , 81 , 3},
+ {33 , 49 , 37 , 53 , 12 , 0}
+};
+
+ cout<<"la matriz 6x6 es: "<<endl;
+ for(int i=0; i<6; i++){
+ 	for(int j=0; j<6; j++){
+ 		cout<<matriz[i][j]<<" ";
+	 }
+	 cout<<endl;
+ }
+ 
+ cout<<endl;
+ cout<<"la matriz reflejada es: "<<endl;
+ for(int i=0; i<6; i++){
+ 	for(int j=5; j>=0; j--){
+ 		cout<<matriz[i][j]<<" ";
+	 }
+	 cout<<endl;
+ }
+ 
+ return 0;
 } 

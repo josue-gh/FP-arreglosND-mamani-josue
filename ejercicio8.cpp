ae2f2d575fdebaa2704c14c6b4597ed9e70c8c85
josue <josue.mlpro.2014@gmail.com>
2025-07-05 21:33:01 -0500

    avance 2- ejercicio 8 - mostrando matriz 5x5

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 main()
 {
+ int matriz[5][5] = {
+ {1, 2, 3, 4, 5 },
+ {6, 7, 8, 9, 0 },
+ {1, 2, 3, 4, 5 },
+ {6, 7, 8, 9, 0 },
+ {1, 2, 3, 4, 5 }
+ };
  
+ cout<<"la matriz 5x5 es: \n";
+ for(int i=0; i<5; i++){
+ 	for(int j=0; j<5; j++){
+ 		cout<<matriz[i][j]<<" ";
+	 }
+	 cout<<endl;
+ }
+ 
+ 
+ 
+ return 0;
 } 

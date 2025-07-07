7d7f3c38434e7c59fe9092a57a01d3e1eaf40c10
josue <josue.mlpro.2014@gmail.com>
2025-07-07 10:27:25 -0500

    ejercicio 1 -version final - imprimir matriz en orden horizontal

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,22 +5,23 @@
  	int matriz_1[3][3];
  	int matriz_2[3][3];
  	//valores de la matriz 1
- 	cout<<ingrese los valores de las matriz 1: <<endl;
+ 	cout<<"ingrese los valores de las matriz 1: "<<endl;
  	for(int i=0; i<3; i++){
  		for(int j=0; j<3; j++){
- 			cout<<ingrese el valor de la posicion <<i+1 << , << j+1<< : ;
+ 			cout<<"ingrese el valor de la posicion "<<i+1 <<" , "<< j+1<<" : ";
  			cin>>matriz_1[i][j];
 		 }
 		 cout<<endl;
 	 }
 	 //valores de la matriz 2
 	 cout<<endl;
-	 cout<<ingrese los valores de la matriz 2: <<endl;
+	 cout<<"ingrese los valores de la matriz 2: "<<endl;
 	 for(int o=0; o<3; o++){
 	 	for(int s=0; s<3; s++){
-	 		cout<<los datos de la matriz 2: <<o+1<< , <<s+1<< : ;
+	 		cout<<"ingrese el valor de la posicion "<<o+1<<" , "<<s+1<<" : ";
 	 		cin>> matriz_2[o][s];
 		 }
+		 cout<<endl;
 	 }
 	 int suma[3][3];
 	 for(int i=0; i<3; i++){
@@ -29,19 +30,22 @@
 	 		suma[i][j]= matriz_1[i][j] + matriz_2[i][j];
 		 }
 	 }
+	 
+	 cout<<endl;
+	 cout<<endl;
 	 	for(int i=0; i<3; i++){
 	 		
  		for(int j=0; j<3; j++){
- 			cout<<matriz_1[i][j]<< ;
+ 			cout<<matriz_1[i][j]<<" " ;
 		 }
-	    cout<< ;
+	    cout<< " ";
 	    
  		for(int j=0; j<3; j++){
- 			cout<<matriz_2[i][j]<< ;
+ 			cout<<matriz_2[i][j]<<" " ;
 		 }
-		cout<< ;
+		cout<< " ";
 		for(int j=0; j<3; j++){
-			cout<<suma[i][j]<< ;
+			cout<<suma[i][j]<<" " ;
 		}
 		cout<<endl;
 	 }
